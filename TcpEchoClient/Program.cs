e8ec016479a10979473865f4b18ed5d11b0c1a94
Anders Børjesson <anbo@zealand.dk>
2021-08-25 12:30:37 +0200

    Add project files.

---
+++ TcpEchoClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TcpEchoClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is the client");
+            Console.WriteLine("Which message to send to the server");
+            string message = Console.ReadLine();
+            Console.WriteLine("Your message: " + message);
+            TcpClient socket = new TcpClient("localhost", 7);
+            NetworkStream ns = socket.GetStream();
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+            writer.WriteLine(message);
+            writer.Flush();
+            string messageReceived = reader.ReadLine();
+            Console.WriteLine("Received: " + messageReceived);
+            socket.Close();
+        }
+    }
+}
