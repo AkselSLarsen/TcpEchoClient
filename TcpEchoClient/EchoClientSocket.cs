b09df1bd5accfec2d129085616fa3c9d9835a9f6
AkselSLarsen <akse2669@edu.easj.dk>
2021-09-05 12:51:17 +0200

    Initial functionality completed

---
+++ TcpEchoClient/EchoClientSocket.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TcpEchoClient {
+    public class EchoClientSocket {
+        public TcpClient Client;
+        public StreamReader Reader;
+        public StreamWriter Writer;
+
+        public EchoClientSocket(string host, int port) {
+            Console.WriteLine("This is the Client");
+
+            Client = new TcpClient(host, port);
+            NetworkStream ns = Client.GetStream();
+            Reader = new StreamReader(ns);
+            Writer = new StreamWriter(ns);
+        }
+
+        public void Run() {
+            Boolean end = false;
+
+            while (!end) {
+                try {
+                    string message = Console.ReadLine();
+                    if (message.ToLower().Contains("/q")) {
+                        end = true;
+                        /*
+                    } else if(message.ToLower().Contains("nr.")) {
+                        PrintNumbers();
+                        */
+                    } else {
+                        Writer.WriteLine(message);
+                        Writer.Flush();
+
+                        ReadLines();
+                    }
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Client.Close();
+        }
+
+        public void ReadLines() {
+            Console.WriteLine("Receiving: " + Reader.ReadLine());
+
+            if(Reader.Peek() >= 0) {
+                ReadLines();
+            }
+        }
+
+        /*
+        private void PrintNumbers() {
+            Task[] tasks = new Task[1000];
+
+            for (int i = 0; i < tasks.Length; i++) {
+                string message = "" + i;
+
+                tasks[i] = new Task(() => {
+                    Writer.WriteLine(message);
+                    Writer.Flush();
+                });
+                tasks[i].Start();
+            }
+
+            for (int i = 0; i < tasks.Length; i++) {
+                while (!tasks[i].IsCompleted) {
+                    Thread.Sleep(10);
+                }
+            }
+        }
+        */
+    }
+}
--- TcpEchoClient/Program.cs
+++ TcpEchoClient/Program.cs
@@ -2,25 +2,15 @@
 using System.IO;
 using System.Net.Sockets;
 
-namespace TcpEchoClient
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("This is the client");
-            Console.WriteLine("Which message to send to the server");
-            string message = Console.ReadLine();
-            Console.WriteLine("Your message: " + message);
-            TcpClient socket = new TcpClient("localhost", 7);
-            NetworkStream ns = socket.GetStream();
-            StreamReader reader = new StreamReader(ns);
-            StreamWriter writer = new StreamWriter(ns);
-            writer.WriteLine(message);
-            writer.Flush();
-            string messageReceived = reader.ReadLine();
-            Console.WriteLine("Received: " + messageReceived);
-            socket.Close();
+namespace TcpEchoClient {
+    public class Program {
+
+        private static string host = "localhost";
+        private static int port = 7;
+
+        public static void Main() {
+            EchoClientSocket socket = new EchoClientSocket(host, port);
+            socket.Run();
         }
     }
 }
